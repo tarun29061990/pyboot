67260230739cd203cdb251ee4f8a25f4c1bf429b
rajeev <rajeev1982@gmail.com>
2016-08-18 10:10:06 +0530

    Added auto detection of fields

--- pyboot/model.py
+++ pyboot/model.py
@@ -1,17 +1,180 @@
+from sqlalchemy import Boolean
 from sqlalchemy import Column
+from sqlalchemy import Date
+from sqlalchemy import DateTime
+from sqlalchemy import Float
 from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Query
+from sqlalchemy.orm import RelationshipProperty
 from sqlalchemy.orm import Session
 
-from json import JSONSerializable
-from util.common import Parser, DatetimeUtil, Validator, DateUtil
+from pyboot.json import JSONSerializable
+from pyboot.util.common import Parser, DatetimeUtil, Validator, DateUtil
+
+TYPE_INT = "int"
+TYPE_FLOAT = "float"
+TYPE_BOOL = "bool"
+TYPE_STR = "str"
+TYPE_DATETIME = "datetime"
+TYPE_DATE = "date"
+TYPE_ENUM = "enum"
+TYPE_OBJ = "obj"
+TYPE_UNKNOWN = "unknown"
 
 
 class Model(JSONSerializable):
     _fields = None
 
+    @classmethod
+    def _get_fields(cls):
+        return cls._fields
+
+    def _to_dict_field(self, obj_dict, name: str, type: str = None):
+        if not name or name not in self.__dict__: return
+        value = getattr(self, name)
+        if type == TYPE_STR:
+            obj_dict[name] = Parser.str(value)
+        elif type == TYPE_INT:
+            obj_dict[name] = Parser.int(value)
+        elif type == TYPE_FLOAT:
+            obj_dict[name] = Parser.float(value)
+        elif type == TYPE_BOOL:
+            obj_dict[name] = Parser.bool(value)
+        elif type == TYPE_DATETIME:
+            if isinstance(value, str):
+                obj_dict[name] = DatetimeUtil.iso_to_dt_local(value)
+            else:
+                obj_dict[name] = Validator.datetime(value)
+        elif type == TYPE_DATE:
+            if isinstance(value, str):
+                obj_dict[name] = DateUtil.iso_to_date(value)
+            else:
+                obj_dict[name] = Validator.date(value)
+        elif type == TYPE_UNKNOWN:
+            obj_dict[name] = value
+        else:
+            obj_dict[name] = value
+
+    def _from_dict_field(self, obj_dict: dict, name: str, type: str = None):
+        if not name or not obj_dict or name not in obj_dict: return
+        value = obj_dict.get(name)
+        if type == TYPE_STR:
+            setattr(self, name, Parser.str(value))
+        elif type == TYPE_INT:
+            setattr(self, name, Parser.int(value))
+        elif type == TYPE_FLOAT:
+            setattr(self, name, Parser.float(value))
+        elif type == TYPE_BOOL:
+            setattr(self, name, Parser.bool(value))
+        elif type == TYPE_DATETIME:
+            if isinstance(value, str):
+                setattr(self, name, DatetimeUtil.iso_to_dt_local(value))
+            else:
+                setattr(self, name, Validator.datetime(value))
+        elif type == TYPE_DATE:
+            if isinstance(value, str):
+                setattr(self, name, DateUtil.iso_to_date(value))
+            else:
+                setattr(self, name, Validator.date(value))
+        elif type == TYPE_UNKNOWN:
+            setattr(self, name, value)
+        else:
+            setattr(self, name, value)
+
+    def to_dict(self):
+        obj_dict = {}
+        if self.id: obj_dict["id"] = self.id
+
+        fields = self.__class__._get_fields()
+        if not fields: return obj_dict
+        for field_name in fields.keys():
+            self._to_dict_field(obj_dict, field_name, fields[field_name])
+
+        return obj_dict
+
+    def from_dict(self, obj_dict: dict):
+        if not obj_dict: return
+        if "id" in obj_dict: self.id = obj_dict["id"]
+
+        fields = self.__class__._get_fields()
+        if not fields: return self
+        for field_name in fields:
+            self._from_dict_field(obj_dict, field_name, fields[field_name])
+
+        return self
+
+    def to_json_dict(self, include: list = None) -> dict:
+        json_dict = self.to_dict()
+
+        fields = self.__class__._get_fields()
+        if not fields: return json_dict
+        for field_name in fields.keys():
+            if fields[field_name] == TYPE_OBJ:
+                self._include_obj(json_dict, include, field_name)
+        return json_dict
+
+    def from_json_dict(self, json_dict: dict):
+        self.from_dict(json_dict)
+
+        fields = self.__class__._get_fields()
+        if not fields: return self
+        for field_name in fields:
+            if fields[field_name] == TYPE_OBJ:
+                self._exclude_obj(json_dict, field_name)
+        return self
+
+    def _include_obj(self, json_dict: dict, include: list, name: str):
+        if not include: include = []
+        field_name = name + "_id"
+
+        sub_obj = getattr(self, name, None) if name in include else None
+        value = getattr(self, field_name, None)
+        if sub_obj:
+            if isinstance(sub_obj, Model): json_dict[name] = sub_obj.to_dict()
+        elif value:
+            json_dict[name] = {"id": value}
+        elif name in include:
+            json_dict[name] = None
+
+        if field_name in json_dict: del json_dict[field_name]
+
+    def _exclude_obj(self, json_dict: dict, name: str):
+        if not json_dict: return
+        if name in json_dict and "id" in json_dict[name]:
+            setattr(self, name + "_id", json_dict[name]["id"])
+
+
+class DatabaseModel(Model):
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)  # type: int
+
+    @classmethod
+    def _get_fields(cls):
+        if cls._fields: return cls._fields
+        columns = inspect(cls).columns
+        if not columns: return None
+        cls._fields = {}
+        for column in columns:
+            type = column.type
+            if isinstance(type, String):
+                cls._fields[column.key] = TYPE_STR
+            elif isinstance(type, Integer):
+                cls._fields[column.key] = TYPE_INT
+            elif isinstance(type, Float):
+                cls._fields[column.key] = TYPE_FLOAT
+            elif isinstance(type, Boolean):
+                cls._fields[column.key] = TYPE_BOOL
+            elif isinstance(type, DateTime):
+                cls._fields[column.key] = TYPE_DATETIME
+            elif isinstance(type, Date):
+                cls._fields[column.key] = TYPE_DATE
+            elif isinstance(type, RelationshipProperty):
+                cls._fields[column.key] = TYPE_OBJ
+            else:
+                cls._fields[column.key] = TYPE_UNKNOWN
+        return cls._fields
 
     @classmethod
     def _query(cls, query: Query, start: int = None, count: int = None, order_by=None) -> Query:
@@ -40,107 +203,5 @@ class Model(JSONSerializable):
     def join_tables(cls, query: Query, include: list = None) -> Query:
         return query
 
-    def _to_dict_field(self, obj_dict, name: str, type: str = None):
-        if type == TYPE_RELATION or not name or not hasattr(self, name): return
-        value = getattr(self, name, None)
-        if type == TYPE_STR:
-            obj_dict[name] = Parser.str(value)
-        elif type == TYPE_INT:
-            obj_dict[name] = Parser.int(value)
-        elif type == TYPE_FLOAT:
-            obj_dict[name] = Parser.float(value)
-        elif type == TYPE_BOOL:
-            obj_dict[name] = Parser.bool(value)
-        elif type == TYPE_DATETIME:
-            if isinstance(value, str):
-                obj_dict[name] = DatetimeUtil.iso_to_dt_local(value)
-            else:
-                obj_dict[name] = Validator.datetime(value)
-        elif type == TYPE_DATE:
-            if isinstance(value, str):
-                obj_dict[name] = DateUtil.iso_to_date(value)
-            else:
-                obj_dict[name] = Validator.date(value)
-        else:
-            obj_dict[name] = value
 
-    def _from_dict_field(self, obj_dict, name: str, type: str = None):
-        if type == TYPE_RELATION or not name or name not in obj_dict or not hasattr(self, name): return
-        value = obj_dict.get(name)
-        if type == TYPE_STR:
-            setattr(self, name, Parser.str(value))
-        elif type == TYPE_INT:
-            setattr(self, name, Parser.int(value))
-        elif type == TYPE_FLOAT:
-            setattr(self, name, Parser.float(value))
-        elif type == TYPE_BOOL:
-            setattr(self, name, Parser.bool(value))
-        elif type == TYPE_DATETIME:
-            if isinstance(value, str):
-                setattr(self, name, DatetimeUtil.iso_to_dt_local(value))
-            else:
-                setattr(self, name, Validator.datetime(value))
-        elif type == TYPE_DATE:
-            if isinstance(value, str):
-                setattr(self, name, DateUtil.iso_to_date(value))
-            else:
-                setattr(self, name, Validator.date(value))
-        else:
-            setattr(self, name, value)
-
-        return None
-
-    def to_dict(self):
-        obj_dict = {}
-        if self.id: obj_dict["id"] = self.id
-        if self.__class__._fields:
-            for field_name in self.__class__._fields.keys():
-                self._to_dict_field(obj_dict, field_name, self.__class__._fields[field_name])
-        return obj_dict
-
-    def from_dict(self, obj_dict: dict):
-        if not obj_dict: return
-        if "id" in obj_dict: self.id = obj_dict["id"]
-        if self.__class__._fields:
-            for field_name in self.__class__._fields.keys():
-                self._from_dict_field(obj_dict, field_name, self.__class__._fields[field_name])
-        return self
-
-    def to_json_dict(self, include: list = None) -> dict:
-        json_dict = self.to_dict()
-        if self.__class__._fields:
-            for field_name in self.__class__._fields.keys():
-                if self.__class__._fields[field_name] != TYPE_RELATION: continue
-                self._include_obj(json_dict, include, field_name)
-        return json_dict
-
-    def from_json_dict(self, json_dict: dict):
-        self.from_dict(json_dict)
-        if self.__class__._fields:
-            for field_name in self.__class__._fields:
-                if self.__class__._fields[field_name] != TYPE_RELATION: continue
-                self._exclude_obj(json_dict, field_name)
-        return self
-
-    def _include_obj(self, json_dict: dict, include: list, name: str):
-        if not include: include = []
-        field_name = name + "_id"
-
-        sub_obj = getattr(self, name, None) if name in include else None
-        value = getattr(self, field_name, None)
-        if sub_obj:
-            if isinstance(sub_obj, Model): json_dict[name] = sub_obj.to_dict()
-        elif value:
-            json_dict[name] = {"id": value}
-        elif name in include:
-            json_dict[name] = None
-
-        if field_name in json_dict: del json_dict[field_name]
-
-    def _exclude_obj(self, json_dict: dict, name: str):
-        if not json_dict: return
-        if name in json_dict and "id" in json_dict[name]:
-            setattr(self, name + "_id", json_dict[name]["id"])
-
-
-ModelBase = declarative_base(cls=Model)
+DatabaseModelBase = declarative_base(cls=DatabaseModel)
