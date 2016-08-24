4b2142644266b0b4a5bdf6ff543799411586e522
rajeev <rajeev1982@gmail.com>
2016-08-24 09:36:46 +0530

    Updated package structure

--- pyboot/decorator.py
+++ pyboot/decorator.py
@@ -8,7 +8,7 @@ from werkzeug.exceptions import BadRequest
 from pyboot.exception import NotFoundException, InvalidInputException, InvalidValueException, \
     AccessDeniedException, DuplicateValueException, InvalidStateException, AuthFailedException
 from pyboot.model import HttpResponse
-from pyboot.util.json import json_response
+from pyboot.json import json_response
 
 
 class Decorator(object):
